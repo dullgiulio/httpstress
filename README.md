39aecb77a2ecc8955e6f6ac6a7736ad8380a6cab
Vasily Korytov <vasily.korytov@yahoo.com>
2014-03-08 10:31:46 -0800

    Inital commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## httpstress
+httpstress is a Go library for HTTP stress testing.
+It launches one goroutine per concurrent connection.
+
+A CLI is provided in [httpstress-go repo](https://github.com/chillum/httpstress-go.git).
+If you need a CLI utility, install [httpstress-go](https://github.com/chillum/httpstress-go.git),
+not httpstress.
+
+### Installation
+* Install [Go runtime](http://golang.org/doc/install)
+* `go get github.com/chillum/httpstress`
+
+### Docs
+* `godoc github.com/chillum/httpstress`
---
+++ httpstress.go
@@ -0,0 +1,88 @@
+/* httpstress is a library for HTTP stress testing.
+It launches one goroutine per concurrent connection. */
+package httpstress
+/* Copyright 2014 Chai Chillum
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
+import (
+	"errors"
+	"net/http"
+	. "regexp"
+)
+
+/* httpstress.Test launches {conn} goroutines to fetch HTTP locations in {urls} list.
+If {max} is more than {conn}, more goroutines will spawn as other are finished,
+resulting in {max} queries (but no more than {conn} in every moment).
+Returns map: {url}/{failed} or error (failed URL message). */
+func Test(conn int, max int, urls []string) (results map[string]int, err error) {
+	for _, i := range urls {
+		if m, _ := MatchString("^http://", i); !m {
+			err = errors.New("Not a HTTP URL: " + i)
+			return
+		}
+	}
+
+	results = make(map[string]int)
+	failures := make(chan string)
+	finished := make(chan bool)
+	total := len(urls) - 1
+	n := 0
+	i := 0
+
+	go logger(failures, results)
+	for ; i < conn; i++ { // Launch initial workers.
+		go worker(urls[n], failures, finished)
+
+		if n < total {
+			n++
+		} else {
+			n = 0
+		}
+	}
+	for ; i < max; i++ { // Launch more workers as initial finish.
+		if <-finished {
+			go worker(urls[n], failures, finished)
+
+			if n < total {
+				n++
+			} else {
+				n = 0
+			}
+		}
+	}
+	for i := 0; i < conn; i++ { // Wait for active workers.
+		<-finished
+	}
+	return
+}
+
+func logger(failures <-chan string, results map[string]int) {
+	for {
+		select {
+		case url := <-failures:
+			results[url]++
+		}
+	}
+}
+
+func worker(url string, failures chan<- string, finished chan<- bool) {
+	resp, err := http.Get(url)
+	if err != nil {
+		failures <- url
+	}
+	if resp != nil {
+		resp.Body.Close()
+	}
+	finished <- true
+}
